5840ae38bd49846b6eb5cb4bfcee49cc4a574c24
LahcenLamkirich <lahcen.lamkirich@gmail.com>
2022-03-24 20:49:33 +0100

    commit

--- app/src/main/java/com/example/sharedpreferences/MainActivity.java
+++ app/src/main/java/com/example/sharedpreferences/MainActivity.java
@@ -4,25 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     SharedPreferences pref;
-    EditText username ;
+    Button btnSave, btnLoad;
+    EditText username;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // when we clicked the save button
-        pref = getSharedPreferences("mypref", MODE_PRIVATE);
-        SharedPreferences.Editor edit = pref.edit();
+        btnSave = findViewById(R.id.btnSave);
+        btnLoad = findViewById(R.id.btnLoad);
         username = findViewById(R.id.username);
-        String user = username.getText().toString();
-        edit.putString("data", user);
-        edit.commit();
+        // when we clicked the save button
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pref = getSharedPreferences("mypref", MODE_PRIVATE);
+                SharedPreferences.Editor edit = pref.edit();
+                String user = username.getText().toString();
+                edit.putString("data", user);
+                edit.commit();
+            }
+        });
 
-
-
+        btnLoad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userLoad = pref.getString("data","");
+                username.setText(userLoad);
+            }
+        });
     }
 }
