a91f5a6e8b60c928e2a4671634b54c7a7be7ab83
LahcenLamkirich <lahcen.lamkirich@gmail.com>
2022-03-25 01:02:15 +0100

    commit

--- app/src/main/java/com/example/sharedpreferences/MainActivity.java
+++ app/src/main/java/com/example/sharedpreferences/MainActivity.java
@@ -2,45 +2,59 @@ package com.example.sharedpreferences;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     SharedPreferences pref;
     Button btnSave, btnLoad;
-    EditText username, password;
+    EditText email, password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Register Page", Toast.LENGTH_LONG).show();
         btnSave = findViewById(R.id.btnSave);
         btnLoad = findViewById(R.id.btnLoad);
-        username = findViewById(R.id.username);
+        email = findViewById(R.id.email);
         password = findViewById(R.id.password);
+
+
         // when we clicked the save button
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 pref = getSharedPreferences("mypref", MODE_PRIVATE);
                 SharedPreferences.Editor edit = pref.edit();
-                String user = username.getText().toString();
+                String user = email.getText().toString();
                 String pass = password.getText().toString();
-                edit.putString("user", user);
+                edit.putString("email", user);
                 edit.putString("pass", pass);
                 edit.commit();
-                username.setText("");
+                email.setText("");
                 password.setText("");
+                Intent i = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(i);
+
             }
         });
+
+        String emai = pref.getString("email","");
+        if(emai != null){
+            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+            startActivity(intent);
+        }
 
         btnLoad.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 SharedPreferences pref = getSharedPreferences("mypref", MODE_PRIVATE);
-                username.setText(pref.getString("user", ""));
+                email.setText(pref.getString("email", ""));
                 password.setText(pref.getString("pass", ""));
             }
         });
